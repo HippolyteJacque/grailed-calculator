d542ff5d1562f6755b754702cf1eba34e5af8abd
unknown <hippolytejacque@hotmail.fr>
2020-01-27 01:16:38 +0100

    naming, getting fs to read json data file

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // getting data from json, create your own ! check the read me for instructions
-const grailedData = require('../grailedData.json');
+const fs = require('fs');
+let rawdata = fs.readFileSync('grailedData.json');
+let grailedData = JSON.parse(rawdata);
 
 // item sales calc
 function getTotalSold(grailedData){
   let totalEuro = 0;
-  const shippingPrice = grailedData["settings"]["averageShippingPriceEuro"];
+  const shippingPrice = grailedData["settings"]["averageShippingPrice"];
   const fees = (grailedData["settings"]["grailed"]+grailedData["settings"]["paypal"])/100;
-  const dollarToEuro = grailedData["settings"]["averageDollarToEuro"];
+  const convert = grailedData["settings"]["convert"];
 
   const grailedSales = grailedData["itemSold"];
   // get brut gain from grailedSales
   for (let item in grailedSales){
     const totalPaypalDollar = grailedSales[item]["soldFor"] - grailedSales[item]["soldFor"]*fees;
-    const totalPaypalEuro = totalPaypalDollar*dollarToEuro;
+    const totalPaypalEuro = totalPaypalDollar*convert;
     totalEuro += Math.floor(totalPaypalEuro -shippingPrice);
   }
   console.log(totalEuro);
@@ -22,5 +24,5 @@ function getTotalSold(grailedData){
 getTotalSold(grailedData);
 
 
-// function exports
+// function exports for tests
 module.exports = getTotalSold;
